39e3b9135b490b91cd7de877522f5aa7ffeb87a5
kkbox <1614689712@qq.com>
2020-08-13 21:01:10 +0800

    添加404页面

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,11 @@
 <template>
-  
-  <div>404</div>
+  <div>
+    <div>当前页面不存在，网址错误</div>
+    <div ><router-link to="/money">返回首页</router-link></div>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
   export default {
     name: 'NotFound'
   }
